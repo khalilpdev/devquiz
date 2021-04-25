39c1add872ebf4fa62fd74324e27dd86a50da1bf
Leandro Khalil <leandro.khalil@runsistemas.com.br>
2021-04-25 11:22:30 -0300

    temino das configs do level button

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -14,7 +14,26 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBarWidget(),
-      body: LevelButtonWidget(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 18),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            LevelButtonWidget(
+              label: "Fácil",
+            ),
+            LevelButtonWidget(
+              label: "Médio",
+            ),
+            LevelButtonWidget(
+              label: "Difícil",
+            ),
+            LevelButtonWidget(
+              label: "Perito",
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
