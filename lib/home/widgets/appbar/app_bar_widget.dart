68091ec1ff1ff0a5a40cdc6c5694f77f59906dff
Leandro Khalil <leandro.khalil@runsistemas.com.br>
2021-04-25 12:30:09 -0300

    termino do layout do card

--- lib/home/widgets/appbar/app_bar_widget.dart
+++ lib/home/widgets/appbar/app_bar_widget.dart
@@ -20,7 +20,7 @@ class AppBarWidget extends PreferredSize {
                     children: [
                       Text.rich(
                         TextSpan(
-                          text: 'Olá ',
+                          text: 'Olá, ',
                           style: AppTextStyles.title,
                           children: [
                             TextSpan(
