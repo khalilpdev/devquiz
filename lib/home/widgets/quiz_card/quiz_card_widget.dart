8165aca44c932fc05d8b07269b35b4f58ac0128a
Leandro Khalil <leandro.khalil@runsistemas.com.br>
2021-04-26 18:28:15 -0300

    continuacao da parte do layout da pag do quiz e termino da aula 2

--- lib/home/widgets/quiz_card/quiz_card_widget.dart
+++ lib/home/widgets/quiz_card/quiz_card_widget.dart
@@ -1,6 +1,7 @@
 import 'package:devquiz/core/app_colors.dart';
 import 'package:devquiz/core/app_images.dart';
 import 'package:devquiz/core/app_text_styles.dart';
+import 'package:devquiz/shared/widgets/progress_indicator/progress_indicator_widget.dart';
 import 'package:flutter/material.dart';
 
 class QuizCardWidget extends StatelessWidget {
@@ -38,12 +39,7 @@ class QuizCardWidget extends StatelessWidget {
               ),
               Expanded(
                 flex: 2,
-                child: LinearProgressIndicator(
-                  value: 0.3,
-                  backgroundColor: AppColors.chartSecondary,
-                  valueColor:
-                      AlwaysStoppedAnimation<Color>(AppColors.chartPrimary),
-                ),
+                child: ProgressIndicadorWidget(value: 0.3),
               )
             ],
           )
---
+++ lib/shared/widgets/progress_indicator/progress_indicator_widget.dart
@@ -0,0 +1,17 @@
+import 'package:devquiz/core/core.dart';
+import 'package:flutter/material.dart';
+
+class ProgressIndicadorWidget extends StatelessWidget {
+  final double value;
+
+  const ProgressIndicadorWidget({Key? key, required this.value})
+      : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return LinearProgressIndicator(
+      value: value,
+      backgroundColor: AppColors.chartSecondary,
+      valueColor: AlwaysStoppedAnimation<Color>(AppColors.chartPrimary),
+    );
+  }
+}
