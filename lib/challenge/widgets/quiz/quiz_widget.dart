73a30e901fede76dce522410f651019f82b19ba9
Leandro Khalil <leandro.khalil@runsistemas.com.br>
2021-05-14 11:49:46 -0300

    mudar state do quizwidget com clique nas respostas

--- lib/challenge/widgets/quiz/quiz_widget.dart
+++ lib/challenge/widgets/quiz/quiz_widget.dart
@@ -1,12 +1,22 @@
 import 'package:devquiz/challenge/widgets/awnser/awnser_widget.dart';
 import 'package:devquiz/core/app_text_styles.dart';
+import 'package:devquiz/shared/models/awnser_model.dart';
 import 'package:devquiz/shared/models/question_model.dart';
 import 'package:flutter/material.dart';
 
-class QuizWidget extends StatelessWidget {
+class QuizWidget extends StatefulWidget {
   final QuestionModel question;
 
   const QuizWidget({Key? key, required this.question}) : super(key: key);
+
+  @override
+  _QuizWidgetState createState() => _QuizWidgetState();
+}
+
+class _QuizWidgetState extends State<QuizWidget> {
+  int indexSelected = -1;
+
+  AwnserModel awnser(int index) => widget.question.awnsers[index];
 
   @override
   Widget build(BuildContext context) {
@@ -15,16 +25,18 @@ class QuizWidget extends StatelessWidget {
         children: [
           SizedBox(height: 64),
           Text(
-            question.title,
+            widget.question.title,
             style: AppTextStyles.heading,
           ),
           SizedBox(height: 24),
-          ...question.awnsers
-              .map((e) => AwnserWidget(
-                    title: e.title,
-                    isRight: e.isRight,
-                  ))
-              .toList(),
+          for (var i = 0; i < widget.question.awnsers.length; i++)
+            AwnserWidget(
+                anwser: awnser(i),
+                isSelected: indexSelected == i,
+                onTap: () {
+                  indexSelected = i;
+                  setState(() {});
+                }),
         ],
       ),
     );
